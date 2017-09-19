b3fe1d59e456a1dea4559f80c534e5da772d1c50
Haven Barnes <haven38@gmail.com>
2017-09-19 18:16:57 -0500

    small changes

--- WebCrawler.py
+++ WebCrawler.py
@@ -6,92 +6,91 @@ from html.parser import HTMLParser
 # This function gets the raw HTML string given a url
 # returns empty string if there is an HTTP Error
 def GetHTML(URL):
-	try:
-		with urlopen(URL) as f:
-			return f.read().decode("utf-8", errors="replace")
-	except Exception as e:
-		print(str(e))
-		return ""
+    try:
+        with urlopen(URL) as f:
+            return f.read().decode("utf-8", errors="replace")
+    except Exception as e:
+        print(str(e))
+        return ""
 
 # This function uses the HTML Parser class to add all the absolute links on the page to the stack
 def ParseHTML(parser, html, parentURL, parentLevel):
-	# Resets list of links and feeds the HTML to the parser
-	parser.initLinks()
-	parser.feed(html)
+    # Resets list of links and feeds the HTML to the parser
+    parser.initLinks()
+    parser.feed(html)
 
-	SplitResult = urlsplit(parentURL)
+    SplitResult = urlsplit(parentURL)
 
-	# Filters out URLs and adds them to the stack
-	links = parser.getLinks()
-	for link in links:
-		if len(link) > 3 and link[0:4] == "http":	# Absolute URL
-			stack.append((parentLevel + 1, link))
-		elif len(link) > 0 and link[0] == "/":		# Relative URL
-			if len(link) > 1 and link[1] == "/":
-				stack.append((parentLevel + 1, SplitResult[0] + ":" + link))
-			else:
-				stack.append((parentLevel + 1, SplitResult[0] + "://" + SplitResult[1] + link))
+    # Filters out URLs and adds them to the stack
+    links = parser.getLinks()
+    for link in links:
+        if len(link) > 3 and link[0:4] == "http":	# Absolute URL
+            stack.append((parentLevel + 1, link))
+        elif len(link) > 0 and link[0] == "/":		# Relative URL
+            if len(link) > 1 and link[1] == "/":
+                stack.append((parentLevel + 1, SplitResult[0] + ":" + link))
+            else:
+                stack.append((parentLevel + 1, SplitResult[0] + "://" + SplitResult[1] + link))
+
+featureSet = {}
 
 # Extracts unigrams and returns a feature vector
 def ExtractUnigram(url, htmlString):
-	if url in featureSet:
-		return featureSet[url]
-	else:
-		featureSet[url] = {}
+    if url in featureSet:
+        return featureSet[url]
+    else:
+        featureSet[url] = {}
 
-	for feature in list(htmlString):
-		key = str(feature)
-		if key in featureSet[url]:
-			featureSet[url][key] += 1
-		else:
-			featureSet[url][key] = 1
+    for feature in list(htmlString):
+        key = str(feature)
+        if key in featureSet[url]:
+            featureSet[url][key] += 1
+        else:
+            featureSet[url][key] = 1
 
-	return featureSet
+    return featureSet[url]
 
 # This class handles the parsing of the HTML
 # It gets the href value of each of the anchor tags on the page
 class MyHTMLParser(HTMLParser):
-	links = []
-	def initLinks(self):
-		self.links = []
+    links = []
+    def initLinks(self):
+        self.links = []
 
-	def getLinks(self):
-		return self.links
+    def getLinks(self):
+        return self.links
 
-	def handle_starttag(self, tag, attrs):
-		if tag == "a":
-			for attr in attrs:
-				if attr[0] == "href" and attr[1] is not None:
-					self.links.append(attr[1])
-
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for attr in attrs:
+                if attr[0] == "href" and attr[1] is not None:
+                    self.links.append(attr[1])
 
 parser = MyHTMLParser()
-
-featureSet = {}
 
 # Outer loop for the iterative deepening
 level = 4
 for i in range(level):
-	# Initialize the stack with the seed URL
-	stack = [(0,"http://asdf.com/")]
+    # Initialize the stack with the seed URL
+    stack = [(0,"http://asdf.com/")]
 
-	# Start the DFS Search
-	while len(stack) > 0:
-		# Pops off the top node of the stack and gets the raw HTML string
-		node = stack.pop()
-		print("\n" + str(node[0]) + ": " + node[1])
+    # Start the DFS Search
+    while len(stack) > 0:
+        # Pops off the top node of the stack and gets the raw HTML string
+        node = stack.pop()
+        print("\n" + str(node[0]) + ": " + node[1])
 
-		HTMLString = GetHTML(node[1])
+        HTMLString = GetHTML(node[1])
 
-		# Uni-gram feature extraction
-		featureVector = ExtractUnigram(str(node[1]), HTMLString)
-		print("Unigram feature set for " + node[1] + ":\n" + str(featureVector))
+        # Uni-gram feature extraction
+        featureVector = ExtractUnigram(str(node[1]), HTMLString)
+        print("Unigram feature set for " + node[1] + ":\n" + str(featureVector))
 
-		# Save the html into text files
+        # Save the html into text files
 
 
-		# If the max depth has not been reached add the chilren nodes to the stack
-		if node[0] < i:
-			ParseHTML(parser, HTMLString, node[1], node[0])
-		
+        # If the max depth has not been reached add the chilren nodes to the stack
+        if node[0] < i:
+            ParseHTML(parser, HTMLString, node[1], node[0])
+               
 parser.close()
