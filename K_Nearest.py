90031838e7401195bac09d1ce6a7cee7c0303016
Haven Barnes <haven38@gmail.com>
2017-10-17 17:00:43 -0500

    added load_data and init to kNN

--- K_Nearest.py
+++ K_Nearest.py
@@ -5,6 +5,10 @@ from Helpers import euclidian_distance, load_dataset
 class K_Nearest(object):
     """K nearest neighbor classifier"""
 
+    def __init__(self, training_data=[], feature_mask =[1 for _ in range(95)]):
+        self.training_data = training_data
+        self.feature_mask = feature_mask
+
     def get_neighbors(self, k, trainingData, testDataItem):
         distances = []
 
@@ -21,6 +25,11 @@ class K_Nearest(object):
             neighbors.append(distances[i][0])
 
         return neighbors
+
+     def load_data(self, training_data):
+        """Loads new training data"""
+        """training data format [(instance, label),(instance, label),...]"""
+        self.training_data = training_data
 
     def classify(self, instance):
         """Performs classification on the given instance"""
